4bd65c1cdceb4c23e22735d636be27e7e2dedc6b
Dawi Lu <dawi9840@gmail.com>
2021-05-27 16:05:30 +0800

    Update 04.CNN_Sequential_API.py

--- 04.CNN_Sequential_API.py
+++ 04.CNN_Sequential_API.py
@@ -9,7 +9,6 @@ from tensorflow.keras.datasets import cifar10
 physical_devices = tf.config.list_physical_devices('GPU')
 tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
-
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 x_train = x_train.astype("float32") / 255.0 
 x_test = x_test.astype("float32") / 255.0 
